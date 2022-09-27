f54405075448a209da70c5f9181b611c323f2235
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-09-28 00:00:08 +0900

    user앱의 views.py에 user_view, user_follow 함수 추가

--- user/views.py
+++ user/views.py
@@ -53,3 +53,24 @@ def sign_in_view(request):
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
+
+# user/views.py 
+
+@login_required
+def user_view(request):  
+    if request.method == 'GET':
+        # 사용자를 불러오기, exclude와 request.user.username 를 사용해서 '로그인 한 사용자'를 제외하기
+        user_list = UserModel.objects.all().exclude(username=request.user.username)  # 지금 로그인 한 유저이름(내 이름)을 제외한 모든 사용자 리스트를 가져오겠다.
+        return render(request, 'user/user_list.html', {'user_list': user_list})  # html을 user_list랑 같이 보여줄거다 
+
+
+@login_required
+def user_follow(request, id):
+    me = request.user  #나를 설정
+    click_user = UserModel.objects.get(id=id)  #내가 방금 누른 사람
+    if me in click_user.followee.all():  # 내가 누른 사람을 팔로우하는 모든 사람들 중에 내가 들어가 있다면 그 팔로우 리스트에서 나를 뺀다.
+        click_user.followee.remove(request.user)
+    else:
+        click_user.followee.add(request.user)  #없다면 나는 내가 방금 누른 사람들 팔로우 하겠다.
+    return redirect('/user')
