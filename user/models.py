1ef592d9ba81fad3b97ededcda2d63579fc8d703
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-09-27 23:44:30 +0900

    models.py에 팔로잉 기능 추가, 유저데이터 많이 만들기

--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 # Create your models here.
@@ -8,3 +9,4 @@ class UserModel(AbstractUser):
         db_table = "my_user"
 
     bio = models.TextField(max_length=256, default='')
+    follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followee')  # AUTH_USER_MODEL은 우리 유저 모델, 우리의 모델을 우리가 ManyToManyField로 참조하겠다.
