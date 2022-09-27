68d3b5218aa5cf7183bd9deca3ef3420f1deda7f
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-09-28 00:01:20 +0900

    user앱의 urls.py에 user_view와 user_follow의 url 추가

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('sign-up/', views.sign_up_view, name='sign-up'),
     path('sign-in/', views.sign_in_view, name='sign-in'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+    path('user/', views.user_view, name = 'user_list'),
+    path('user/follow/<int:id>', views.user_follow, name='user-follow'),
 ]
