ce1352cebdd694a0b7ad0bcee0a6fa29bcc96804
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-09-28 19:20:04 +0900

    github에 업로드 하기 위해 database, migrations를 삭제하고 settings.py를 수정

--- spartasns/settings.py
+++ spartasns/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rvx*0y)5i9kmr8@-dzlfg8x$%&#f2&tn@(0@gw*r^4uzen)g$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
