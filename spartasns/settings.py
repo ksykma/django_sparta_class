18c2edae7f79527cb92a6e73e4c549b1fdc4a0f2
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-09-28 14:45:53 +0900

    태그를 작성할 수 있게 하는 모듈 다운 받고 settings.py와 tweet의  models.py를 이에 맞게 수정

--- spartasns/settings.py
+++ spartasns/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'user',
     'tweet',
     'restaurant',
+    'taggit.apps.TaggitAppConfig',
+    'taggit_templatetags2',
 ]
+
+TAGGIT_CASE_INSENSITIVE = True
+TAGGIT_LIMIT = 50
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- tweet/models.py
+++ tweet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user.models import UserModel
+from taggit.managers import TaggableManager  # 우리가 우리 글에 태그를 추가해 줄 수 있게 만들어주는 매니저
 
 
 # Create your models here.
@@ -9,6 +10,7 @@ class TweetModel(models.Model):
 
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     content = models.CharField(max_length=256)
+    tags = TaggableManager(blank=True)  # 이게 비어있어도 작동하겠다.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
